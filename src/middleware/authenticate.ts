67b5771641e6b20b682977c436b771f38e4556a0
Joe Meyer <joseph@meyer.id>
2023-04-11 12:11:22 -0500

    logs auth

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -2,19 +2,33 @@ import { Request, Response, NextFunction } from 'express';
 import basicAuth from 'express-basic-auth';
 import { verifyIdToken } from './verifyIdToken';
 
-const apiUsers = {
-    KwisatzHaderach: process.env.API_TOKEN || '',
-    // Add more users and their secrets as needed
+type ApiUsersType = {
+  [key: string]: string;
+};
+
+const apiUsers: ApiUsersType = {
+  KwisatzHaderach: process.env.API_TOKEN || '',
+  // Add more users and their secrets as needed
 };
 
 export const authenticate = [
     (req: Request, res: Response, next: NextFunction) => {
-    const authType = req.header('Authorization')?.split(' ')[0];
-
-    if (authType === 'Basic') {
-      basicAuth({
-        users: apiUsers,
-        unauthorizedResponse: { message: 'Unauthorized' },
+      const authHeader = req.header('Authorization');
+      console.log('Authorization header received in server:', authHeader);
+  
+      const authType = authHeader?.split(' ')[0];
+  
+      if (authType === 'Basic') {
+        basicAuth({
+          authorizeAsync: true,
+          authorizer: async (username, password, callback) => {
+            console.log('Username from basicAuth:', username);
+            console.log('Password from basicAuth:', password);
+    
+            const userPassword = apiUsers[username];
+            callback(null, !!userPassword && userPassword === password);
+          },
+          unauthorizedResponse: { message: 'Unauthorized' },  
       })(req, res, next);
     } else {
       verifyIdToken()(req, res, next);
