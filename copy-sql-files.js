460a72a3b22b90e1852cdc37bc054bfb86fc2435
Joe Meyer <joseph@meyer.id>
2024-01-14 01:39:45 +0000

    copy sql files needs to stay old

--- copy-sql-files.js
+++ copy-sql-files.js
@@ -1,25 +1,25 @@
-import { readdirSync, existsSync, mkdirSync, copyFileSync } from 'fs';
-import { join, extname } from 'path';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const path = require('path');
 
-const srcDir = join(__dirname, 'src', 'database');
-const destDir = join(__dirname, 'dist', 'database');
+const srcDir = path.join(__dirname, 'src', 'database');
+const destDir = path.join(__dirname, 'dist', 'database');
 
 function copySqlFiles(src, dest) {
-  const entries = readdirSync(src, { withFileTypes: true });
+  const entries = fs.readdirSync(src, { withFileTypes: true });
 
   entries.forEach((entry) => {
-    const srcPath = join(src, entry.name);
-    const destPath = join(dest, entry.name);
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
 
     if (entry.isDirectory()) {
-      if (!existsSync(destPath)) {
-        mkdirSync(destPath);
+      if (!fs.existsSync(destPath)) {
+        fs.mkdirSync(destPath);
       }
       copySqlFiles(srcPath, destPath);
-    } else if (entry.isFile() && extname(entry.name) === '.sql') {
-      copyFileSync(srcPath, destPath);
+    } else if (entry.isFile() && path.extname(entry.name) === '.sql') {
+      fs.copyFileSync(srcPath, destPath);
     }
   });
 }
-
-copySqlFiles(srcDir, destDir);
