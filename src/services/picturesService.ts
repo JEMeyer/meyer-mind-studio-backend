3b7ce7377f65dfcc27d1e032ee12d0cec5552bf5
Joe Meyer <joseph@meyer.id>
2024-01-16 09:54:47 +0000

    fix picture service too

--- src/services/picturesService.ts
+++ src/services/picturesService.ts
@@ -20,13 +20,14 @@ export const addPicture = async (
 export const getPictureById = async (pictureId: string, userId?: string) => {
   const sql = `
       WITH vote_summary AS (
-          SELECT picture_id, SUM(value) as total_votes
+          SELECT id_value, SUM(value) as total_votes
           FROM votes
-          GROUP BY picture_id
+          WHERE id_type = 2
+          GROUP BY id_value
       )
-      SELECT p.id, p.public_path, p.prompt, p.created_at, vs.total_votes, uv.value as user_vote
+      SELECT p.id, p.public_path, p.prompt, p.created_at, p.type, vs.total_votes, uv.value as user_vote
       FROM pictures p
-      LEFT JOIN vote_summary vs ON p.id = vs.picture_id
+      LEFT JOIN vote_summary vs ON p.id = vs.id_value
       LEFT JOIN votes uv ON p.id = uv.picture_id AND uv.user_id = ${userId}
       WHERE v.id = ${pictureId};
     `;
